c5c137fb6cc7d13b9b233ca09fd53ec8bab635e7
kelvin <kelvingalotog@hotmail.com>
2018-09-23 21:11:26 -0300

    Teste Alpes One Desenvolvedor Front End

---
+++ validacao.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	$('#item1').click(function(){
+		$('#cliqueborda2').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda3').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda4').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda5').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda1').css("border-left","8px solid #A9F5E1")
+	})
+	$('#item2').click(function(){
+		$('#cliqueborda1').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda3').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda4').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda5').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda2').css("border-left","8px solid #A9F5E1");
+	})
+	$('#item3').click(function(){
+		$('#cliqueborda1').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda2').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda4').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda5').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda3').css("border-left","8px solid #A9F5E1");
+	})
+	$('#item4').click(function(){
+		$('#cliqueborda1').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda2').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda3').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda5').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda4').css("border-left","8px solid #A9F5E1");	
+	})
+	$('#item5').click(function(){
+		$('#cliqueborda1').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda2').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda3').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda4').css("border-left","8px solid #FFFFFF");
+		$('#cliqueborda5').css("border-left","8px solid #A9F5E1");	
+	})
+});
